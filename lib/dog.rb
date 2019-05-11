2b61ebded6d7202f93de048bde8aeb56e8a9e8a5
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-11 22:45:21 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+    def name
+    @name
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
+
+
+
+class Person
+  def name=(new_name)
+    @name = new_name
+  end
+  
+    def name
+    @name
+  end
+  
+  def job=(good_job)
+    @job = good_job
+  end
+  
+  def job
+    @job
+  end
+end
