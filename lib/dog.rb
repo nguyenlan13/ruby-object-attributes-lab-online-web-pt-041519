55a82e5ec14d617ccdde23254ce2b725baeb1333
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-11 22:45:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,3 +19,20 @@ end
 
 
 
+class Person
+  def name=(new_name)
+    @name = new_name
+  end
+  
+    def name
+    @name
+  end
+  
+  def job=(good_job)
+    @job = good_job
+  end
+  
+  def job
+    @job
+  end
+end
